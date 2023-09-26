ee2710c50dd6d7ef3bfbff18c070be6a651a1cb7
lupingp <294402584@qq.com>
2023-09-27 01:01:26 +0800

    增加获取IP

--- mc_jacoco/src/main/java/com/example/mc_jacoco/util/LocalIpUtil.java
+++ mc_jacoco/src/main/java/com/example/mc_jacoco/util/LocalIpUtil.java
@@ -1,12 +1,23 @@
 package com.example.mc_jacoco.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author luping
  * @date 2023/9/23 14:33
  */
+@Slf4j
 public class LocalIpUtil {
 
     public static String getBaseUrl(){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        try {
+            String localIp = GetIPAddressUtil.getLinuxIpAddress();
+            sb.append("http://").append(localIp).append(":8899/");
+            return sb.toString();
+        } catch (Exception e) {
+            log.error("【获取本机机器ip出现异常...】【异常原因：{}】",e.getMessage());
+        }
+        return sb.toString();
     }
 }
